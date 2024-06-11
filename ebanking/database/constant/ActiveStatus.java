e53d96dd2efdb5f014c1a1ce11e643c144a1ac6d
DangAnhKhoa <120726333+khoa031102@users.noreply.github.com>
2024-06-11 13:41:49 +0700

    Delete ebanking directory

--- ebanking/database/constant/ActiveStatus.java
+++
@@ -1,5 +0,0 @@
-package com.example.ebanking.database.constant;
-
-public enum ActiveStatus implements EnumDescription {
-    DISABLED, INACTIVE, ACTIVE
-}
--- ebanking/database/constant/EnumDescription.java
+++
@@ -1,9 +0,0 @@
-package com.example.ebanking.database.constant;
-
-public interface EnumDescription {
-    default String getDescription(){
-        return this.getClass().getSimpleName() + "." + this.name() + ".description";
-    }
-
-    String name();
-}
